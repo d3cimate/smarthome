a9b8aeb18984bf212bcec986502cca0499a332ac
d3cimate <80759269+d3cimate@users.noreply.github.com>
2023-09-18 16:52:38 +0400

    Changes

--- login-page.js
+++ login-page.js
@@ -2,15 +2,19 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
-loginButton.addEventListener("click", (e) => {
+loginButton.addEventListener("click", (e) => 
+{
     e.preventDefault();
-    const username = loginForm.username.value;
+    const ipaddress = loginForm.ipaddress.value;
     const password = loginForm.password.value;
 
-    if (username === "user" && password === "web_dev") {
+    if (ipaddress === "192.168.1.100" && password === "raspberry")
+    {
         alert("You have successfully logged in.");
         location.reload();
-    } else {
+    }
+    else
+    {
         loginErrorMsg.style.opacity = 1;
     }
 })
