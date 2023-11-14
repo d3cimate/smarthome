75b2b8f09df903702504de3fc884124cb62bafd0
d3cimate <mohamed.m.eldaba@gmail.com>
2023-11-14 13:11:14 +0400

    Update changeintensity.js

--- changeintensity.js
+++ changeintensity.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
 // import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-analytics.js";
-import { setDoc, collection, getFirestore, doc, getDocs, getDoc } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js";
+import { updateDoc, setDoc, collection, getFirestore, doc, getDocs, getDoc } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js";
 
 window.changeIntensity = async function (roomnum, intensity)
 {
@@ -12,7 +12,7 @@ window.changeIntensity = async function (roomnum, intensity)
     {
         const lightRef = collection(db, "lightrequests");
 
-        await setDoc(doc(lightRef, "lightrequests"), 
+        await updateDoc(doc(lightRef, "lightrequests"), 
         {
             roomnum: roomnum, on: true, intensity: intensity
         });
